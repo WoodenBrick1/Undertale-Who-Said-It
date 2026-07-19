1ad4b364e4cc37679d5ffa315dc8e6be64758e05
WoodenBrick1 <brickwooden@gmail.com>
2026-07-19 23:39:39 +0300

    Fixed Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,43 @@ function App () {
   const [dialogue, setDialogue] = useState("")
   const [characterSelected, setCharacterSelected] = useState("")
   const [guessedCorrectly, setGuessedCorrectly] = useState(null)
+  const [numOfGuesses, setNumOfGuesses] = useState(0)
 
+  const gameOver = guessedCorrectly || numOfGuesses == 2
+  console.log(gameOver)
   console.log("Rendering: App")
 
+
+  if (gameOver && characterSelected !== "") {
+    setCharacterSelected("")
+  }
 
   const GetRandomDialogue = () => {
     setDialogue(allText[Math.floor(Math.random() * allText.length)])
   }
 
+
   const Guess = () => {
+
+    if (characterSelected === "") {
+      return
+    }
+
     if (characterSelected === dialogue.character) {
       setGuessedCorrectly(true)
     } else {
       setGuessedCorrectly(false)
     }
+
+    setNumOfGuesses(numOfGuesses + 1)
+    setCharacterSelected("")
+  }
+
+  const Reset = () => {
+    GetRandomDialogue()
+    setNumOfGuesses(0)
+    setCharacterSelected("")
+    setGuessedCorrectly(null)
   }
 
   if (dialogue == "") {
@@ -41,10 +64,13 @@ function App () {
       <TextBox text={dialogue.dialogue} />
 
       <div id="button-container">
-        <Button name="GUESS" onClick={Guess}></Button>
+        {!gameOver ?
+          <Button name="GUESS" onClick={Guess}></Button> :
+          <Button name="PLAY AGAIN" onClick={Reset} />}
       </div>
 
-      <GuessText guessedCorrectly={guessedCorrectly} characterSelected={characterSelected} correctCharacter={dialogue.character} />
+      <GuessText guessedCorrectly={guessedCorrectly} characterSelected={characterSelected} correctCharacter={dialogue.character} gameOver={gameOver} />
+
       <CharactersButtons characters={allCharacters} setCharacterSelected={setCharacterSelected} />
     </div>
   )
--- src/components/GuessText.jsx
+++ src/components/GuessText.jsx
@@ -1,22 +1,23 @@
 import "../styles/GuessText.css"
 
-function GuessText ({ guessedCorrectly, characterSelected, correctCharacter }) {
-    console.log("Rendering: GuessText, Props: ", guessedCorrectly, characterSelected, correctCharacter)
+function GuessText ({ guessedCorrectly, characterSelected, correctCharacter, gameOver }) {
+    console.log("Rendering: GuessText, Props: ", guessedCorrectly, characterSelected, correctCharacter, gameOver)
 
     let text = "If you see this, something went rly wrong"
-    if (guessedCorrectly === null) {
 
-        if (characterSelected === "") {
+    if (characterSelected === "") {
+        if (guessedCorrectly == null) {
             text = "Please Select a Character"
+        } else if (guessedCorrectly) {
+            text = `You guessed correctly!!!`
+        } else if (!gameOver) {
+            text = `You we're wrong!`
         } else {
-            text = `Selected: ${characterSelected}`
+            text = `You we're wrong! It was ${correctCharacter}`
         }
-    } else if (guessedCorrectly) {
-        text = `You guessed correctly!!!`
     } else {
-        text = `You we're wrong! It was ${correctCharacter}`
+        text = `Selected: ${characterSelected}`
     }
-
 
     return (
         <p id="guess-text">{text}</p>

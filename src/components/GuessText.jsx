09ba27228fcee532df77d39a41e9095622d4a5fb
WoodenBrick1 <brickwooden@gmail.com>
2026-07-24 20:44:28 +0300

    Coloured Text

--- src/components/GuessText.jsx
+++ src/components/GuessText.jsx
@@ -4,23 +4,27 @@ function GuessText ({ guessedCorrectly, characterSelected, correctCharacter, gam
     console.log("Rendering: GuessText, Props: ", guessedCorrectly, characterSelected, correctCharacter, gameOver)
 
     let text = "If you see this, something went rly wrong"
+    let color = ""
 
     if (characterSelected === "") {
         if (guessedCorrectly == null) {
             text = "Please Select a Character"
         } else if (guessedCorrectly) {
             text = `You guessed correctly!!!`
+            color = "green"
         } else if (!gameOver) {
             text = `You we're wrong!`
+            color = "red"
         } else {
             text = `You we're wrong! It was ${correctCharacter}`
+            color = "red"
         }
     } else {
         text = `Selected: ${characterSelected}`
     }
 
     return (
-        <p id="guess-text">{text}</p>
+        <p id="guess-text" className={color}>{text}</p>
     )
 }
 
